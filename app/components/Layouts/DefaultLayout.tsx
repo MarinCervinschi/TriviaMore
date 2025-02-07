634cd9c6ca715343d1368fa341db6552f82a68e0
MarinCervinschi <marincervinschi3@gmail.com>
2025-02-07 15:52:36 +0100

    fix: bg overflow

--- app/components/Layouts/DefaultLayout.tsx
+++ app/components/Layouts/DefaultLayout.tsx
@@ -32,21 +32,21 @@ export default function DefaultLayout({
   return (
     <>
       {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className="absolute h-full w-full z-[-100]">
-        <Squares
-          speed={0.2}
-          squareSize={40}
-          direction='diagonal' // up, down, left, right, diagonal
-          borderColor='#f5f5f5'
-          hoverFillColor='#f5f5f5'
-        />
-      </div>
       <div className="flex flex-col min-h-screen bg-transparent text-gray-900">
         {/* <!-- ===== Header Start ===== --> */}
         <Header />
         {/* <!-- ===== Header End ===== --> */}
 
         {/* <!-- ===== Main Content Start ===== --> */}
+        <div className="fixed w-full h-full z-[-100]">
+          <Squares
+            speed={0.2}
+            squareSize={40}
+            direction='diagonal' // up, down, left, right, diagonal
+            borderColor='#f5f5f5'
+            hoverFillColor='#f5f5f5'
+          />
+        </div>
         <main className="flex-grow flex flex-col justify-center items-center my-6">
           {children}
           {showScrollArrow && (
