a4da0a42b8da3b8ae2b8a6ec5085bf6974aefafe
MarinCervinschi <marincervinschi3@gmail.com>
2025-08-12 11:51:02 +0200

    formating

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,15 +1,15 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-gray-600/10 dark:bg-gray-100/10", className)}
-      {...props}
-    />
-  )
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div
+			className={cn(
+				"animate-pulse rounded-md bg-gray-600/10 dark:bg-gray-100/10",
+				className
+			)}
+			{...props}
+		/>
+	);
 }
 
-export { Skeleton }
+export { Skeleton };
